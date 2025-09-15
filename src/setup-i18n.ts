ca1ae9c65ab0ccf4f87c787cce95631f9153cb84
Royston Shufflebotham <royston@shufflebotham.org>
2025-09-15 23:32:17 +0100

    Initialize project with TypeScript, i18next localization, and configuration files

---
+++ src/setup-i18n.ts
@@ -0,0 +1,37 @@
+import i18next from "i18next";
+import mainEn from "./nls/en/main.json" with { type: "json" };
+import mainFr from "./nls/fr/main.json" with { type: "json" };
+import mainEnGb from "./nls/en-GB/main.json" with { type: "json" };
+import mainEnUs from "./nls/en-US/main.json" with { type: "json" };
+import mainPl from './nls/pl/main.json' with { type: "json" };
+
+
+declare module "i18next" {
+  interface CustomTypeOptions {
+    defaultNS: "main";
+    compatibilityJSON: "v4";
+    resources: {
+      main: typeof mainEn;
+    };
+  }
+}
+
+export async function setupI18N() {
+  const promise = i18next.init({
+    debug: true,
+    fallbackLng: "en",
+    // i18nFormat: {
+    //   parseLngForICU(_language: string) {
+    //     return i18next.language;
+    //   },
+    // },
+  });
+
+  i18next.addResourceBundle("en", "main", mainEn);
+  i18next.addResourceBundle("en-GB", "main", mainEnGb);
+  i18next.addResourceBundle("en-US", "main", mainEnUs);
+  i18next.addResourceBundle("fr", "main", mainFr);
+  i18next.addResourceBundle("pl", "main", mainPl);
+
+  await promise;
+}
