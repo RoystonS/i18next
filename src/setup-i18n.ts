8f84cc893184838a0315190ae914e6eaff50dc2b
Royston Shufflebotham <royston@shufflebotham.org>
2025-09-16 18:21:41 +0100

    Switch base en to ICU format

--- src/setup-i18n.ts
+++ src/setup-i18n.ts
@@ -1,10 +1,11 @@
 import i18next from "i18next";
+import ICU from "i18next-icu";
+
 import mainEn from "./nls/en/main.json" with { type: "json" };
 import mainFr from "./nls/fr/main.json" with { type: "json" };
 import mainEnGb from "./nls/en-GB/main.json" with { type: "json" };
 import mainEnUs from "./nls/en-US/main.json" with { type: "json" };
 import mainPl from './nls/pl/main.json' with { type: "json" };
-
 
 declare module "i18next" {
   interface CustomTypeOptions {
@@ -17,14 +18,14 @@ declare module "i18next" {
 }
 
 export async function setupI18N() {
-  const promise = i18next.init({
+  const promise = i18next.use(ICU).init({
     debug: true,
     fallbackLng: "en",
-    // i18nFormat: {
-    //   parseLngForICU(_language: string) {
-    //     return i18next.language;
-    //   },
-    // },
+    i18nFormat: {
+      parseLngForICU(_language: string) {
+        return i18next.language;
+      },
+    },
   });
 
   i18next.addResourceBundle("en", "main", mainEn);
